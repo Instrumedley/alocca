a55924febf9a426a7a4b519e10cee31aa9fea67a
rafael <rafaelalfredo2008@gmail.com>
2019-11-18 21:36:28 +0100

    adding a comment

--- alocca/settings.py
+++ alocca/settings.py
@@ -84,14 +84,15 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
+    # I used MySQL initially so I'll leave this here to make it easy
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'anocca',
-    #     'USER': 'root',
-    #     'PASSWORD': 'gourmetli',
-    #     'HOST': '127.0.0.1',
+    #     'NAME': 'alocca',
+    #     'USER': '',
+    #     'PASSWORD': '',
+    #     'HOST': '',
     #     'PORT': '',
-    #     'DATABASE': 'anocca',
+    #     'DATABASE': 'alocca',
     # }
 }
 
